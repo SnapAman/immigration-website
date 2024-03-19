b7bb7340652a735684c7ddcb0052cc8a68a33923
SnapAfiya <155967079+SnapAfiya@users.noreply.github.com>
2024-03-19 21:53:11 +0530

    About Us page completed

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,8 +1,106 @@
 // AboutUs.js
 import React from 'react';
-
+import '../AboutUs/AboutUs.css';
+import Footer from '../Footer/Footer';
 function AboutUs() {
-  return <h2>AboutUs</h2>;
+  return (
+    <>
+      <div className="AboutBackground">
+        <div className="txt1">
+        </div>
+      </div>
+
+      <div className="header-body">
+        <div className="image-sec">
+          <div className="image-sec1">
+            <img src="../src/assets/About-img1.jpg" alt="abc" className='im1' />
+          </div>
+          <div className="image-sec2">
+            <div>
+              <img src="../src/assets/About-img2.jpg" alt="pqr" className='im2' />
+            </div>
+            <div>
+              <img src="../src/assets/About-img3.jpg" alt="xyz" className='im3' />
+            </div>
+          </div>
+          <img src="../src/assets/center.svg" className='centerImg' alt="" />
+        </div>
+
+
+        <div className="content-sect">
+          <h1 className='title-head'>Experienced Lawyers Provide Immigration Services.</h1>
+          <p className='para'>We provide legal professionals who specialize in immigration law and offer their expertise to individuals or entities navigating immigration processes.We provide comprehensive support to ensure compliance with immigration requirements, streamline the application process, and facilitate successful placements, enabling businesses to access top talent globally and individuals to pursue rewarding career opportunities abroad."</p>
+          <div className="mission-vision">
+            <div className="icon">
+              <svg width="90" height="90" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 20C0 8.9543 8.95431 0 20 0H80C91.0457 0 100 8.95431 100 20V80C100 91.0457 91.0457 100 80 100H20C8.9543 100 0 91.0457 0 80V20Z" fill="#D9D9D9" fill-opacity="0.44" />
+                <path d="M59.75 66.3881C60.35 66.3881 60.875 66.1873 61.325 65.7857C61.775 65.3842 62 64.9157 62 64.3804C62 63.845 61.775 63.3765 61.325 62.975C60.875 62.5734 60.35 62.3727 59.75 62.3727C59.15 62.3727 58.625 62.5734 58.175 62.975C57.725 63.3765 57.5 63.845 57.5 64.3804C57.5 64.9157 57.725 65.3842 58.175 65.7857C58.625 66.1873 59.15 66.3881 59.75 66.3881ZM68 66.3881C68.6 66.3881 69.125 66.1873 69.575 65.7857C70.025 65.3842 70.25 64.9157 70.25 64.3804C70.25 63.845 70.025 63.3765 69.575 62.975C69.125 62.5734 68.6 62.3727 68 62.3727C67.4 62.3727 66.875 62.5734 66.425 62.975C65.975 63.3765 65.75 63.845 65.75 64.3804C65.75 64.9157 65.975 65.3842 66.425 65.7857C66.875 66.1873 67.4 66.3881 68 66.3881ZM76.25 66.3881C76.85 66.3881 77.375 66.1873 77.825 65.7857C78.275 65.3842 78.5 64.9157 78.5 64.3804C78.5 63.845 78.275 63.3765 77.825 62.975C77.375 62.5734 76.85 62.3727 76.25 62.3727C75.65 62.3727 75.125 62.5734 74.675 62.975C74.225 63.3765 74 63.845 74 64.3804C74 64.9157 74.225 65.3842 74.675 65.7857C75.125 66.1873 75.65 66.3881 76.25 66.3881ZM68 77.765C63.85 77.765 60.313 76.4596 57.389 73.8487C54.465 71.2378 53.002 68.0817 53 64.3804C53 60.6773 54.463 57.5212 57.389 54.912C60.315 52.3029 63.852 50.9975 68 50.9957C72.15 50.9957 75.688 52.3011 78.614 54.912C81.54 57.5229 83.002 60.679 83 64.3804C83 68.0835 81.537 71.2404 78.611 73.8513C75.685 76.4623 72.148 77.7668 68 77.765ZM35 40.2879H65V34.9341H35V40.2879ZM49.025 72.4112H29C27.35 72.4112 25.938 71.8874 24.764 70.8398C23.59 69.7922 23.002 68.5314 23 67.0573V29.5802C23 28.1079 23.588 26.8479 24.764 25.8004C25.94 24.7528 27.352 24.2281 29 24.2263H71C72.65 24.2263 74.063 24.751 75.239 25.8004C76.415 26.8497 77.002 28.1097 77 29.5802V47.5157C75.55 46.891 74.087 46.4226 72.611 46.1103C71.135 45.798 69.598 45.6418 68 45.6418C67.45 45.6418 66.937 45.6525 66.461 45.6739C65.985 45.6953 65.498 45.7516 65 45.8426V45.6418H35V50.9957H53.375C52.475 51.7541 51.663 52.5795 50.939 53.4718C50.215 54.3642 49.577 55.3234 49.025 56.3495H35V61.7034H47.225C47.125 62.1496 47.063 62.585 47.039 63.0098C47.015 63.4345 47.002 63.8914 47 64.3804C47 65.8527 47.15 67.225 47.45 68.4975C47.75 69.7699 48.275 71.0745 49.025 72.4112Z" fill="black" />
+              </svg>
+            </div>
+
+            <div className="mission-content">
+              <h1 className='t-head'>Our Mission:</h1>
+              <p className='t-body'>Our mission is to provide immigration legal services with compassion,  and advocacy.</p>
+            </div>
+          </div>
+
+          <div className="mission-vision">
+            <div className="icon">
+              <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 20C0 8.9543 8.95431 0 20 0H80C91.0457 0 100 8.95431 100 20V80C100 91.0457 91.0457 100 80 100H20C8.9543 100 0 91.0457 0 80V20Z" fill="#D9D9D9" fill-opacity="0.64" />
+                <path d="M25.4545 23.9707H74.5455C75.9921 23.9707 77.3795 24.5974 78.4024 25.7131C79.4253 26.8287 80 28.3417 80 29.9194V65.6119C80 68.8837 77.5455 71.5607 74.5455 71.5607H60.9091V77.5094H39.0909V71.5607H25.4545C22.4545 71.5607 20 68.8837 20 65.6119V29.9194C20 26.6179 22.4273 23.9707 25.4545 23.9707ZM25.4545 29.9194V65.6119H74.5455V29.9194H25.4545ZM41.8182 38.8426H58.1818V56.6888H41.8182V38.8426Z" fill="black" />
+              </svg>
+            </div>
+
+            <div className="mission-content">
+              <h1 className='t-head'>Our Vision:</h1>
+              <p className='t-body'>Our vision is to foster communities by facilitating integration and equitable opportunities for immigrants.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="scrollable-div">
+        <div className="our-team">
+          <h1 className='team-head'>Meet Our Team:</h1>
+          <div className="member">
+            <div className="member-img">
+              <img src="../src/assets/Member1.jpg" alt="abc" className='mimage1' />
+            </div>
+            <div className="member-info">
+              <h1 className='mtitle'>Alastair McClymont</h1>
+              <p className='mpara'>Alastair McClymont has experience in Immigration Law since 1997. He began work in Auckland as a lawyer in 1996 for a large litigation and immigration law firm. By 2000 Alastair was specialising solely in immigration work including refugee applications and appeals and appearance on immigration reviews in the New Zealand High Court and Court of Appeal. Alastair set up his own firm in 2001 under the name Monk and McClymont, a two lawyer firm based in central Auckland concentrating solely on immigration. In 2005 Alastair began working on his own as a barrister and solicitor continuing to concentrate solely on immigration work. Since 2005 the firm has expanded and now operates under the name McClymont and Associates. In what spare time Alastair can get he enjoys sailing, mountain biking, travel and spending time with his wife and young children..</p>
+            </div>
+          </div>
+
+          <div className="member">
+            <div className="member-info">
+              <h1 className='mtitle'>Aakanksha McClymont</h1>
+              <p className='mpara'>Aakanksha McClymont has been working with Alastair in this firm for more than 10 years. Originally from Chandigarh in India she graduated with a BA in Psychology but is most proud of her Master's degree in Common Sense. Aakanksha is the point of contact for most clients of the firm looking out for their best interests, helping them navigate the difficulties of the immigration process and managing the workload of the lawyers and admin staff working on the files. Aakanksha loves being busy and when not running the office she loves travel, socialising, exercising, friends and family.</p>
+            </div>
+            <div className="member-img">
+              <img src="../src/assets/Member2.jpg" alt="abc" className='mimage1' />
+            </div>
+          </div>
+
+          <div className="member">
+            <div className="member-img">
+              <img src="../src/assets/Member3.jpg" alt="abc" className='mimage1' />
+            </div>
+            <div className="member-info">
+              <h1 className='mtitle'>Lydia Li</h1>
+              <p className='mpara'>With 7 years of experience in the legal sector, Lydia Li is a dedicated expert in immigration matters. Residing in New Zealand for two decades, she has made this beautiful country her home. Lydia holds a bachelor degree from the University of Auckland and is currently working towards her LLB degree. Her journey also includes a decade of valuable experience in large corporations, during which she cultivated strong customer relationships.
+                Lydia's exceptional expertise shines through her impressive track record of successfully handling complex cases, such as s61, ministerial directions, and exceptions in immigration instructions. Her profound understanding of immigration policy, combined with her proficiency in Mandarin, English, and Cantonese, empowers her to effectively assist clients from diverse backgrounds. You can reach her at lydia@amlaw.co.nz </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default AboutUs;
