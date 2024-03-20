d98337c9e6677dae95fb56222023983b7d8e60d1
Aman khan <aman.khan@snapfluencetech.com>
2024-03-20 17:02:09 +0530

    Wednesday commit

--- src/Components/VisaInfo/VisaInfoTabs.jsx
+++ src/Components/VisaInfo/VisaInfoTabs.jsx
@@ -8,27 +8,92 @@ import { Tab as BaseTab, tabClasses } from '@mui/base/Tab';
 import '../VisaInfo/VisaInfoTabs.css';
 import ReplyIcon from '@mui/icons-material/Reply';
 import studentvisa from '../../assets/StudentVisa.jpg'
+import workvisa from '../../assets/WorkVisa.jpg';
+import partnerVisa from '../../assets/partnerVisa.jpg';
+import parentsVisa from '../../assets/parentsVisa.jpg';
+import visitorVisa from '../../assets/visitorVisa.jpg';
 
 export default function VisaInfoTabs() {
   return (
     <Tabs defaultValue={0} orientation="vertical">
       <TabsList>
-        <Tab> <div className='tabContent'>Resident Visa<span><ReplyIcon /></span></div> </Tab>
-        <Tab><div className='tabContent'>Student Visa<span><ReplyIcon /></span></div></Tab>
-        <Tab><div className='tabContent'>Tourist Visa<span><ReplyIcon /></span></div></Tab>
+        <Tab> <div className='tabContent'>Essential Work Visa / Resident Visa<span><ReplyIcon /></span></div> </Tab>
+        <Tab><div className='tabContent'>Partnership Work / Resident Visa<span><ReplyIcon /></span></div></Tab>
+        <Tab><div className='tabContent'>Parent Visitor / Resident Visa<span><ReplyIcon /></span></div></Tab>
+        <Tab><div className='tabContent'>Student Visa (Onshore/Offshore)<span><ReplyIcon /></span></div></Tab>
         <Tab><div className='tabContent'>Visitor Visa<span><ReplyIcon /></span></div></Tab>
-        <Tab><div className='tabContent'>Work Visa<span><ReplyIcon /></span></div></Tab>
-        <Tab><div className='tabContent'>Post-Study Visa<span><ReplyIcon /></span></div></Tab>
       </TabsList>
       <TabPanel value={0}>
+      <div class="student-visa">
+          <h1>Essential Work Visa / Resident Visa</h1>
+        </div>
+        <div className="VisaInfocContainer">
+          <div className="image-container">
+            <img src={workvisa} alt=" " />
+          </div>
+          <div className="info-container">
+            <div className='info-box'>
+              <h3>About</h3>
+              <p>Essential Work Visa / Resident Visa allows individuals to work and reside in a foreign country based on their essential skills or qualifications. It facilitates workforce mobility to fill critical roles in sectors experiencing shortages or high demand.</p>
+            </div>
+            <div className='info-box'>
+            <h3>Eligibility Criteria</h3>
+            <p>Eligibility criteria typically include possessing relevant skills or qualifications in demand, securing a job offer from an approved employer, meeting health and character requirements, and demonstrating the ability to support oneself financially during the stay.</p>
+            </div>
+            <Button variant="contained" disableElevation className="grayButton">Learn more</Button>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel value={1}>
         <div class="student-visa">
-          <h1>STUDENT VISA</h1>
+          <h1>Partnership Work/Resident Visa</h1>
+        </div>
+        <div className="VisaInfocContainer">
+          <div className="image-container">
+            <img src={partnerVisa} alt=" " />
+          </div>
+          <div className="info-container">
+            <div className='info-box'>
+              <h3>About</h3>
+              <p>Partnership work/resident visas are designed for individuals in committed relationships with New Zealand citizens or residents. These visas enable partners to live, work, and study in New Zealand while supporting their partner's application for residency.</p>
+            </div>
+            <div className='info-box'>
+            <h3>Eligibility Criteria</h3>
+            <p>Eligibility typically requires proof of a genuine and stable relationship, such as cohabitation or marriage, with a New Zealand citizen or resident. Applicants must meet health and character requirements and demonstrate financial stability to support themselves during their stay in New Zealand.</p>
+            </div>
+            <Button variant="contained" disableElevation className="grayButton">Learn more</Button>
+          </div>
+        </div>
+        </TabPanel>
+      <TabPanel value={2}>
+      <div class="student-visa">
+          <h1>Parent Visitor/Resident Visa</h1>
+        </div>
+        <div className="VisaInfocContainer">
+          <div className="image-container">
+            <img src={parentsVisa} alt=" " />
+          </div>
+          <div className="info-container">
+            <div className='info-box'>
+              <h3>About</h3>
+              <p>This visa allows parents to visit or reside with their children who are citizens or residents of a particular country. It enables family reunification and facilitates spending extended periods of time together.</p>
+            </div>
+            <div className='info-box'>
+            <h3>Eligibility Criteria</h3>
+            <p>Eligibility typically requires proof of relationship, financial stability, health insurance, and sometimes evidence of accommodation. Some countries may impose additional criteria, such as income thresholds or sponsorship requirements.</p>
+            </div>
+            <Button variant="contained" disableElevation className="grayButton">Learn more</Button>
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel value={3}>
+      <div class="student-visa">
+          <h1>Student Visa</h1>
         </div>
         <div className="VisaInfocContainer">
           <div className="image-container">
             <img src={studentvisa} alt=" " />
           </div>
-          {/* Div with information about Student Visa */}
           <div className="info-container">
             <div className='info-box'>
               <h3>About</h3>
@@ -37,40 +102,32 @@ export default function VisaInfoTabs() {
             <div className='info-box'>
             <h3>Eligibility Criteria</h3>
             <p>Eligibility Criteria:Applicants must have acceptance from a recognized educational institution, financial stability, valid passport, health requirements fulfilled, and a clean criminal record.</p>
-            
             </div>
             <Button variant="contained" disableElevation className="grayButton">Learn more</Button>
           </div>
         </div>
-        {/* <div>
-          <div>
-
-          </div>
-          <div>
-            <div></div>
-            <div></div>
-          </div>
-          </div>
-
-          <img class="visa-image" src={studentvisa} alt="Student Visa" />
-
-          <div class="info-container">
-            <div class="info-box">
-              <h3>About</h3>
-              <p>Our student visa service is designed to assist individuals who wish to pursue their academic goals abroad. Whether you're planning to enroll in a university, college we provide support to help you with application.</p>
-            </div>
-
-            <div class="info-box">
-              <h3>Eligibility Criteria</h3>
-              <p>Applicants must have acceptance from a recognized educational institution, financial stability, valid passport, health requirements fulfilled, and a clean criminal record.</p>
-            </div>
-          </div> */}
       </TabPanel>
-      <TabPanel value={1}>Second page</TabPanel>
-      <TabPanel value={2}>Third page</TabPanel>
-      <TabPanel value={3}>Fourth page</TabPanel>
-      <TabPanel value={4}>Fifth page</TabPanel>
-      <TabPanel value={5}>Sixth page</TabPanel>
+      <TabPanel value={4}>
+      <div class="student-visa">
+          <h1>Visitor VISA</h1>
+        </div>
+        <div className="VisaInfocContainer">
+          <div className="image-container">
+            <img src={visitorVisa} alt=" " />
+          </div>
+          <div className="info-container">
+            <div className='info-box'>
+              <h3>About</h3>
+              <p>A visitor visa allows individuals to travel to another country for tourism, visiting family or friends, attending business meetings, or medical treatment. It's typically a temporary authorization granted by the destination country's government.</p>
+            </div>
+            <div className='info-box'>
+            <h3>Eligibility Criteria</h3>
+            <p>Eligibility criteria for a visitor visa often include having a valid passport, demonstrating sufficient funds to cover expenses during the visit, providing evidence of ties to the home country to ensure the applicant's return, and not having a criminal record or any immigration violations.</p>
+            </div>
+            <Button variant="contained" disableElevation className="grayButton">Learn more</Button>
+          </div>
+        </div>
+      </TabPanel>
     </Tabs>
   );
 }
@@ -121,6 +178,7 @@ const TabsList = styled(BaseTabsList)(
   display: flex;
   padding: 15px;
   gap: 40px;
+  height : 75vh;
   flex-direction: column;
   align-items: center;
   justify-content: center;
